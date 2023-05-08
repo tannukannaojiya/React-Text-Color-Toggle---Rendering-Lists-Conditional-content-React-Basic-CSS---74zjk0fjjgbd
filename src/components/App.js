19dfb7f9bccd4d6e12d35619b0e735a7ee181c45
tannu kannaojiya <113445718+tannukannaojiya@users.noreply.github.com>
2023-05-08 12:04:00 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
@@ -10,7 +10,7 @@ const handleClick=()=>{
 
   return (
     <div id="main">
-      <p className={`${(changeColor===true)?'blueColor':'redColor'}`} >Newton School</p>
+      <p className={changeColor===true?'blueColor':'redColor'} >Newton School</p>
       <button id='button' onClick={handleClick}>Change Style</button>
     </div>
   )
